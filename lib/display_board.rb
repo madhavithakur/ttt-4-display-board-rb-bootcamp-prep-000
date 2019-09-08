4c466d9959d78bec863461f82c045d71c733a8d1
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 02:57:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,17 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-# def display_board(board)
-#   board = [" "," "," "," "," "," "," "," "," "]
-#   # puts  board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-#   # puts  board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-#   board[0] = "O"
-#   board[4] = "X"
-#   puts board
-# end 
-
-def display_board(board)	# Define display_board that accepts a board and prints
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "	# out the current state.
+def display_board(board)	
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "	
   puts "-----------"	
   puts " #{board[3]} | #{board[4]} | #{board[5]} "	
   puts "-----------"	
