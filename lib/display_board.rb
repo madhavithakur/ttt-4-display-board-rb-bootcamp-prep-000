4928d43b9742335ae16699c8e250846784d78cf9
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 02:56:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,3 +9,11 @@
 #   board[4] = "X"
 #   puts board
 # end 
+
+def display_board(board)	# Define display_board that accepts a board and prints
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "	# out the current state.
+  puts "-----------"	
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "	
+  puts "-----------"	
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "	
+end
